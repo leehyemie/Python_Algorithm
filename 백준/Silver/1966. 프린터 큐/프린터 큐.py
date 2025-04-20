40c1d64b2283b03e7174a4c4d86eb7792a4c1e5d
leehyemie <80510889+leehyemie@users.noreply.github.com>
2025-04-20 20:56:18 +0900

    Update 프린터 큐.py

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -6,30 +6,37 @@ for _ in range(t):
     n, m = map(int, input().split(' '))
     arr = list(map(int, input().split(' ')))
 
-    # dic = {arr[i]: i for i in range(len(arr))}
-    queue = deque([(i, p) for i, p in enumerate(arr)])
-    
+   # (중요도, 문서)
+    queue = deque([(p, i) for i, p in enumerate(arr)])   
     count = 0
 
     # 큐가 비면 탈출
     while queue:
         # 일단 꺼냄
-        current = queue.popleft()
+        current = queue.popleft() # current = (1, 0)
         # 더 중요한 문서가 있는지
-        higher = False
+        lower = False
 
         for item in queue:
-            if item[1] > current[1]:
-                higher = True
+            # 더 중요한 문서가 존재
+            if item[0] > current[0]:
+                lower = True
                 break
         
         # 더 중요한 문서가 있다
-        if higher:
+        if lower:
             # 뒤로 가셈
             queue.append(current)
         else: # 아니다 내가 제일 중요하다
             count += 1 # 출력
             
-            if current[0] == m:
+            # 타겟 문서인지 확인
+            if current[1] == m:
                 print(count)
                 break
+
+
+
+    
+
+
